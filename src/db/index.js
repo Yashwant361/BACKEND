a625e8cfd4d8cfec96ee1f490fa481b383829fd4
yashwantray361 <yashwantray361@gmail.com>
2026-03-10 11:29:29 +0530

    add prettier config

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants"
+
+const connectDB = async () => {
+    try {
+        const connectionInstnace = await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+            console.log(`\n MongoDB connected !! DB HOST:
+                ${connectionInstnace.connection.host}`);
+            
+    } catch (error) {
+        console.log('MONGODB connection error', error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
