904b892f7338c65ea4964c975bd1003012b1999f
yashwantray361 <yashwantray361@gmail.com>
2026-03-12 13:27:43 +0530

    file upload using cloudinary

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import CookiesParser from 'cookies-parser'
+import cookieParser from "cookie-parser";
 
 const app = express()
 
@@ -12,6 +12,6 @@ app.use((cors({
 app.use(express.json({ limit: process.env.JSON_LIMIT }))
 app.use(express.urlencoded({extended: true, limit: process.env.JSON_LIMIT}))
 app.use(express.static("public"))
-app.use(CookiesParser())
+app.use(cookieParser());
 
 export { app }; //export the app
