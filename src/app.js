fb2de7491b14bbcdbf400830258b545becfd5d3f
yashwantray361 <yashwantray361@gmail.com>
2026-03-14 12:07:58 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: process.env.JSON_LIMIT}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+ 
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
 export { app }; //export the app
--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ connectDB()
 
         app.on("Error", (error) => {
             console.log("Error:", error)
-            throw err
-
+            throw error
         })
 
         app.listen(process.env.PORT || 8000, () => {
@@ -30,37 +29,3 @@ connectDB()
 
 
 
-
-
-
-
-
-
-
-    
-/* 
-// Approach 1 :
-import  express from 'express'
-const app = express()
-    (async () => {
-
-        try {
-            await mongoose.connect(`${process.env.
-                MONGODB_URI}/${DB_NAME}`)
-            app.on('error', (error) => {
-                console.log('Error: ', error);
-                throw error
-            })
-
-            app.listen(process.env.PORT, () => {
-                console.log(`App is listening on port ${process.env.PORT}`);
-            })
-
-
-        } catch (error) {
-            console.error('Error: ', error)
-            throw err
-        }
-})()
-
-*/
